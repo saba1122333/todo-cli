ac549dab2108485636df7adf59f04c7c8ef6bb5a
saba1122333 <tchumburidzesaba@gmail.com>
2025-08-02 16:52:05 +0400

    Remove unused utils package reference from main.go

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/saba1122333/todo-cli/cli"
-	"github.com/saba1122333/todo-cli/utils"
 	"os"
 )
 
 func main() {
-	utils.CheckOrCreate()
 	err := cli.Run()
 	if err != nil {
 		fmt.Printf("Error : %v\n", err)
